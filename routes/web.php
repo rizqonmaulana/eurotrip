63373161df2213b0c29532d8a04a9b3ee73d4605
rizqonmaulana <rizqonmaulana5@gmail.com>
2020-09-27 22:37:49 +0800

    add Travel CRUD

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,8 @@ Route::prefix('admin')
     ->middleware(['auth', 'admin'])
     ->group(function () {
         Route::get('/', 'DashboardController@index')->name('dashboard');
+
+        Route::resource('travel-package', 'TravelPackageController');
     });
 
 Auth::routes(['verify' => true]);
